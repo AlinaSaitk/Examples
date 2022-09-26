f44b6ee38c30acf8cc2d2c3e54ad5b22318f4a50
Alina <aax_74@mail.ru>
2022-09-26 16:19:52 +0500

    add c#_1

---
+++ c#_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу которая на вход принимает число 
+// и выдает ее квадрат
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num*num;
+Console.WriteLine($"Результат: {result}");
+
+if (num == result)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
