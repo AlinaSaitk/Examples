eb19ea05232d67145932753c3e1652c7b520d0ed
Alina <aax_74@mail.ru>
2022-09-30 12:21:13 +0500

    add c#_3_22

---
+++ c#_3_22/Program.cs
@@ -0,0 +1,10 @@
+﻿// 22. Напишите программу, которая принимает на вход 
+// число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1, 4.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++) Console.Write($"{Math.Pow(i, 2)}, ");
