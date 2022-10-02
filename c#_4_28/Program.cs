d868e52bad11b1dc07b117daac47c0b2b156ff2d
AlinaTT <aax_74@mail.ru>
2022-10-02 23:02:49 +0500

    add c#_4_28

---
+++ c#_4_28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int sum = 1;
+for (int i = 1; i <= n; i++)
+{
+    sum = sum*i;
+}
+Console.WriteLine($"{n} -> {sum}");
