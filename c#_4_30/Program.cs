51debec560b655a95a32ed56ffd567b652849871
Alina <aax_74@mail.ru>
2022-10-04 15:22:10 +0100

    add c#_4_30

---
+++ c#_4_30/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 30. Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+Console.Clear();
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(0, 2);
+    if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+    else Console.WriteLine(arr[i]);
+}
