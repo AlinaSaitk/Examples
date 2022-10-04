3527900823bb58372296110316345c0d8519912c
Alina <aax_74@mail.ru>
2022-10-04 17:42:04 +0100

    add c#_4_25_dz

---
+++ c#_4_25_dz/Program.cs
@@ -0,0 +1,24 @@
+﻿//ДЗ - Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+
+for (int i = 0; i < b; i++) sum = sum * a;
+
+Console.WriteLine($"{a}, {b} -> {sum}");
+
+
+// Console.Clear();
+// Console.Write("Введите число А: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число В: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"{a}, {b} -> {Math.Pow(a, b)}");
