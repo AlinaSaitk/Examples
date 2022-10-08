7b5be5e5ea06db653151c6dc79e04b5f58844d38
Alina <aax_74@mail.ru>
2022-10-08 10:53:27 +0100

    c#_5_32

---
+++ c#_5_32/Program.cs
@@ -0,0 +1,32 @@
+﻿// 32. Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4,8,-8, -2]
+
+
+int[] ChangeSign(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+Console.Clear();
+int[] arr = new int[] {-4, -8, 8, 2};
+PrintArray(arr);
+Console.Write($" -> ");
+ChangeSign(arr);
+PrintArray(arr);
+Console.WriteLine();
