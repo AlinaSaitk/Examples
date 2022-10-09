390e3ea529fef575d06f6b04bc81739990312818
Alina <aax_74@mail.ru>
2022-10-09 12:16:32 +0100

    c#_5_36_dz сумма элементов, стоящих на нечётных позициях

---
+++ c#_5_36_dz/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] RandomArray(int n, int from, int to)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(from, to);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+void OddPosSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+        sum = sum + array[i];
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Clear();
+int[] arr = RandomArray(7, -99, 100);
+PrintArray(arr);
+Console.Write(" -> ");
+OddPosSum(arr);
+
