eaebc3fc30323c8c87b3092d0e7626f2b2f886c6
AlinaTT <aax_74@mail.ru>
2022-10-09 23:55:44 +0500

    c#_6_41_dz Кол-во эл > 0

---
+++ c#_6_41_dz/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+Console.Write("Сколько чисел вы будете вводить?: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа по порядку: ");
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0) count++;
+}
+Console.WriteLine($"-> {count}");
+
+
