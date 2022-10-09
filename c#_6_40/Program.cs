3aa96d4ecae5feff6e88642ecad122f4b0834b05
AlinaTT <aax_74@mail.ru>
2022-10-09 20:43:37 +0500

    c#_6_40 проверка треугольника

---
+++ c#_6_40/Program.cs
@@ -0,0 +1,27 @@
+﻿// 40. Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+int[] InputArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < 3; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.Clear();
+Console.WriteLine("Введите три числа, стороны треугольника");
+int[] arr = InputArray(3);
+
+if (arr[0] >= arr[1] + arr[2] || 
+    arr[1] >= arr[0] + arr[2] || 
+    arr[2] >= arr[0] + arr[1]) 
+{
+    Console.WriteLine("Такого треугольника не существует");
+}
+else
+{
+    Console.WriteLine("Треугольник существует");
+}
