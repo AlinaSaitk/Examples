085ecc8cf1c868611f01f1f8bc69a4271a16befe
AlinaTT <aax_74@mail.ru>
2022-10-14 00:23:26 +0500

    c#_9_63 рекурсия - числа по порядку

---
+++ c#_9_63/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.WriteLine("Введите число N: ");
+// int n = int.Parse(Console.ReadLine()!);
+
+void NaturalNumb(int numb)
+{
+
+    if (numb > 0)
+    {
+        NaturalNumb(numb - 1);
+        Console.Write($"{numb}" + " ");
+        
+    }
+}
+
+NaturalNumb(5);
+
+// Факториал
+// int Fact(int n)
+// {
+//     if (n == 1) return 1;
+//     return n * Fact(n - 1);
+// }
+
+// int x = Fact(5);
+// Console.WriteLine(x);
