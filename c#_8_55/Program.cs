5d9caf42ace1128e0e78e102878ceb1fc25dc145
Alina <aax_74@mail.ru>
2022-10-13 10:14:33 +0500

    add c#_8_55 заменить строки на столбцы

--- c#_8_55/Program.cs
+++ c#_8_55/Program.cs
@@ -1,47 +1,62 @@
 ﻿// 55. Задайте двумерный массив. Напишите программу, 
 // которая заменяет строки на столбцы. В случае, если 
-// это невозможно, программа должна вывести сообщение для пользователя. (в рамках одгого массива)
+// это невозможно, программа должна вывести сообщение для пользователя. 
+// (в рамках одгого массива)
 
 
-// int[,] CreateRandom2DArray(int row, int col, int from = 0, int to = 10)
+int[,] CreateRandom2DArray(int row, int col, int from = 0, int to = 10)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(from, to);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+
+// void CheckIfCanChange(int[,] array)
 // {
-//     int[,] array = new int[row, col];
-//     for (int i = 0; i < array.GetLength(0); i++)
+//     for (int i = 1; i < array.GetLength(0); i++)
 //     {
-//         for (int j = 0; j < array.GetLength(1); j++)
+//         if (array[i].Length != array[0].Length)
 //         {
-//             array[i, j] = new Random().Next(from, to);
-//         }
-//     }
-//     return array;
-// }
-
-// void Print2DArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     } 
-// }
-
-// void ChangeRows(int[,] array)
-// {
-//     int[] temp = new int[array.GetLength(1)];
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//         temp[j] = array[0, j];
-//         array[0, j] = array[array.GetLength(0)-1, j];
-//         array[array.GetLength(0)-1, j] = temp[j];
+//             Console.WriteLine("Замена невозможна");
+//         } 
 //     }
 // }
 
+int[,] ChangeRowToCol(int[,] array)
+{
+    int[,] array2 = new int[array.GetLength(1),array.GetLength(0)];
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[i, j] = array[j, i];
+        }
+    }
+    return array2;
+}
 
-// int[,] arr = CreateRandom2DArray(3, 4);
-// Print2DArray(arr);
-// Console.WriteLine();
-// ChangeRows(arr);
-// Print2DArray(arr);
+
+
+int[,] arr = CreateRandom2DArray(3, 4);
+Print2DArray(arr);
+Console.WriteLine();
+int [,] arr2 = ChangeRowToCol(arr);
+Print2DArray(arr2);
