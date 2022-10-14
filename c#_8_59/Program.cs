6b2375eede2c9e8430de61edf780309436b8b7a9
Alina <aax_74@mail.ru>
2022-10-14 19:06:23 +0500

    add c#_9_59 Удаление столбцов и строк с мин эл

--- c#_8_59/Program.cs
+++ c#_8_59/Program.cs
@@ -28,63 +28,167 @@ void Print2DArray(int[,] array)
     } 
 }
 
-void DeleteRowColOnMin(int[,] array)
+int[,] DeleteRowColOnMin(int[,] array)
 {
+    int min = array[0, 0];
+    int numb = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == min)
+            {
+                numb++;
+            }
+            if (array[i, j] < min)
+            {
+                numb = 1;
+                min = array[i, j];
+            } 
+        }
+    }
+    Console.WriteLine($"min element = {min}");
+    Console.WriteLine(numb);
 
+    for (int p = 1; p <= numb; p++)
+    {
+        min = array[0, 0];
+        int[] minCoord = new int[2];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] < min)
+                {
+                    minCoord[0] = i;
+                    minCoord[1] = j;
+                } 
+            }
+        }
+
+        int m = 0;
+        int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            if (k != minCoord[0])
+            {
+                int n = 0;
+                for (int l = 0; l < array.GetLength(1); l++)
+                {
+                    if (l != minCoord[1])
+                    {
+                        array2[m, n] = array[k, l];
+                        n++;
+                    }
+                }
+                m++;
+            }
+        }
+        array = array2;
+    }
+    return array;   
 }
+
+
+// int[,] DeleteRowColOnMin(int[,] array)
+// {
+//     int min = array[0, 0];
+//     int numb = 1;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] == min)
+//             {
+//                 numb++;
+//             }
+//             if (array[i, j] < min)
+//             {
+//                 numb = 1;
+//                 min = array[i, j];
+//             } 
+//         }
+//     }
+//     Console.WriteLine($"min element = {min}");
+
+//     int[,] minCoord = new int[numb, 2];
+//     int numbOrder = 0;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] == min)
+//             {
+//                 minCoord[numbOrder, 0] = i;
+//                 minCoord[numbOrder, 1] = j;
+//                 numbOrder++;
+//             }
+//         }
+//     }
+    
+//     // for (int p = 0; p < numb; p++)
+//     // {
+//     //     int m = 0;
+//     //     int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+//     //     for (int k = 0; k < array.GetLength(0); k++)
+//     //     {
+//     //         if (k != minCoord[p, 0])
+//     //         {
+//     //             int n = 0;
+//     //             for (int l = 0; l < array.GetLength(1); l++)
+//     //             {
+//     //                 if (l != minCoord[p, 1])
+//     //                 {
+//     //                     array2[m, n] = array[k, l];
+//     //                     n++;
+//     //                 }
+//     //             }
+//     //             m++;
+//     //         }
+//     //     }
+//     //     int[,] array = array2;
+
+
+
+
+//     int[,] DelMinReq(int [,] array, n)
+//     {
+//         if (n == 0) return array;
+//         else 
+//         {
+//             int m = 0;
+//             int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+//             for (int k = 0; k < array.GetLength(0); k++)
+//             {
+//                 if (k != minCoord[numb-1, 0])
+//                 {
+//                     int n = 0;
+//                     for (int l = 0; l < array.GetLength(1); l++)
+//                     {
+//                         if (l != minCoord[p, 1])
+//                         {
+//                             array2[m, n] = array[k, l];
+//                             n++;
+//                         }
+//                     }
+//                     m++;
+//                 }
+//             }   
+//         }
+//     }
+
+    
+//     return array2;   
+// }
+
 
 int[,] arr = CreateRandom2DArray(5, 4);
 Print2DArray(arr);
-WriteElementsAmount(arr);
+// for (int i = 0; i)
+// int[,] arr2 = DeleteRowColOnMin(arr);
+// Print2DArray(arr2);
 
 
-// 61. Вывести первые N строк треугольника Паскаля.
-// Сделать вывод в виде равнобедренного треугольника.
-
-// ДЗ
-
-// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
-
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
-// Например, задан массив:
-
-// 1 4 7 2
-
-// 5 9 2 3
-
-// 8 4 2 4
-
-// 5 2 6 7
-
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
-
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
-// 01 02 03 04
-// 12 13 14 05
-// 11 16 15 06
-// 10 09 08 07
+arr = DeleteRowColOnMin(arr);
+Console.WriteLine();
+Print2DArray(arr);
