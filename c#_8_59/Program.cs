9a7ede766852d06ecfa9da11d8ee7e52990a0fb2
AlinaTT <aax_74@mail.ru>
2022-10-16 22:08:10 +0500

    c#_8_59 удалит строку и столбец на мин эл

--- c#_8_59/Program.cs
+++ c#_8_59/Program.cs
@@ -28,68 +28,6 @@ void Print2DArray(int[,] array)
     } 
 }
 
-int[,] DeleteRowColOnMin(int[,] array)
-{
-    int min = array[0, 0];
-    int numb = 1;
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if (array[i, j] == min)
-            {
-                numb++;
-            }
-            if (array[i, j] < min)
-            {
-                numb = 1;
-                min = array[i, j];
-            } 
-        }
-    }
-    Console.WriteLine($"min element = {min}");
-    Console.WriteLine(numb);
-
-    for (int p = 1; p <= numb; p++)
-    {
-        min = array[0, 0];
-        int[] minCoord = new int[2];
-        for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
-                if (array[i, j] < min)
-                {
-                    minCoord[0] = i;
-                    minCoord[1] = j;
-                } 
-            }
-        }
-
-        int m = 0;
-        int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
-        for (int k = 0; k < array.GetLength(0); k++)
-        {
-            if (k != minCoord[0])
-            {
-                int n = 0;
-                for (int l = 0; l < array.GetLength(1); l++)
-                {
-                    if (l != minCoord[1])
-                    {
-                        array2[m, n] = array[k, l];
-                        n++;
-                    }
-                }
-                m++;
-            }
-        }
-        array = array2;
-    }
-    return array;   
-}
-
-
 // int[,] DeleteRowColOnMin(int[,] array)
 // {
 //     int min = array[0, 0];
@@ -110,85 +48,135 @@ int[,] DeleteRowColOnMin(int[,] array)
 //         }
 //     }
 //     Console.WriteLine($"min element = {min}");
+//     Console.WriteLine(numb);
 
-//     int[,] minCoord = new int[numb, 2];
-//     int numbOrder = 0;
-//     for (int i = 0; i < array.GetLength(0); i++)
+//     for (int p = 1; p <= numb; p++)
 //     {
-//         for (int j = 0; j < array.GetLength(1); j++)
+//         min = array[0, 0];
+//         int[] minCoord = new int[2];
+//         for (int i = 0; i < array.GetLength(0); i++)
 //         {
-//             if (array[i, j] == min)
+//             for (int j = 0; j < array.GetLength(1); j++)
 //             {
-//                 minCoord[numbOrder, 0] = i;
-//                 minCoord[numbOrder, 1] = j;
-//                 numbOrder++;
+//                 if (array[i, j] < min)
+//                 {
+//                     minCoord[0] = i;
+//                     minCoord[1] = j;
+//                 } 
 //             }
 //         }
-//     }
-    
-//     // for (int p = 0; p < numb; p++)
-//     // {
-//     //     int m = 0;
-//     //     int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
-//     //     for (int k = 0; k < array.GetLength(0); k++)
-//     //     {
-//     //         if (k != minCoord[p, 0])
-//     //         {
-//     //             int n = 0;
-//     //             for (int l = 0; l < array.GetLength(1); l++)
-//     //             {
-//     //                 if (l != minCoord[p, 1])
-//     //                 {
-//     //                     array2[m, n] = array[k, l];
-//     //                     n++;
-//     //                 }
-//     //             }
-//     //             m++;
-//     //         }
-//     //     }
-//     //     int[,] array = array2;
 
-
-
-
-//     int[,] DelMinReq(int [,] array, n)
-//     {
-//         if (n == 0) return array;
-//         else 
+//         int m = 0;
+//         int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+//         for (int k = 0; k < array.GetLength(0); k++)
 //         {
-//             int m = 0;
-//             int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
-//             for (int k = 0; k < array.GetLength(0); k++)
+//             if (k != minCoord[0])
 //             {
-//                 if (k != minCoord[numb-1, 0])
+//                 int n = 0;
+//                 for (int l = 0; l < array.GetLength(1); l++)
 //                 {
-//                     int n = 0;
-//                     for (int l = 0; l < array.GetLength(1); l++)
+//                     if (l != minCoord[1])
 //                     {
-//                         if (l != minCoord[p, 1])
-//                         {
-//                             array2[m, n] = array[k, l];
-//                             n++;
-//                         }
+//                         array2[m, n] = array[k, l];
+//                         n++;
 //                     }
-//                     m++;
 //                 }
-//             }   
+//                 m++;
+//             }
 //         }
+//         array = array2;
 //     }
-
-    
-//     return array2;   
+//     return array;   
 // }
+
+
+int[] FindMinAmount(int[,] array)
+{
+    int[] minArray = new int[2];
+    int min = array[0, 0];
+    int numb = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == min)
+            {
+                numb++;
+            }
+            else if (array[i, j] < min)
+            {
+                numb = 1;
+                min = array[i, j];
+            } 
+        }
+    }
+    minArray[0] = min;
+    minArray[1] = numb;
+    return minArray;
+}
+
+int[] FindElemCoord(int elem, int[,] array)
+{
+    int[] elemCoord = new int[2] {-1, -1};
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == elem)
+            {
+                elemCoord[0] = i;
+                elemCoord[1] = j;
+                break;
+            } 
+        }
+    }
+    return elemCoord;
+}
+
+int[,] DeleteRowCol(int[] elemCoord, int[,] array)
+{
+    if (elemCoord[0] >= 0)
+    {
+        int[,] array2 = new int[array.GetLength(0)-1, array.GetLength(1)-1];
+        int m = 0;
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            if (k != elemCoord[0])
+            {
+                int n = 0;
+                for (int l = 0; l < array.GetLength(1); l++)
+                {
+                    if (l != elemCoord[1])
+                    {
+                        array2[m, n] = array[k, l];
+                        n++;
+                    }
+                }
+                m++;
+            }
+        }
+        return array2;
+    }
+    else return array;
+}
 
 
 int[,] arr = CreateRandom2DArray(5, 4);
 Print2DArray(arr);
-// for (int i = 0; i)
-// int[,] arr2 = DeleteRowColOnMin(arr);
-// Print2DArray(arr2);
 
+int[] minArr = FindMinAmount(arr); 
+Console.WriteLine($"min element = {minArr[0]}");
+Console.WriteLine($"Amount of min element = {minArr[1]}");
 
-arr = DeleteRowColOnMin(arr);
-Console.WriteLine();
-Print2DArray(arr);
+for (int i = 0; i < minArr[1]; i++)
+{
+    Console.WriteLine();
+    int[] elemCoord = FindElemCoord(minArr[0], arr);
+    if (elemCoord[0] < 0) break;
+    Console.WriteLine($"Min coordinates ({elemCoord[0]}, {elemCoord[1]})");
+
+    arr = DeleteRowCol(elemCoord, arr);
+
+    Console.WriteLine();
+    Print2DArray(arr);
+}
