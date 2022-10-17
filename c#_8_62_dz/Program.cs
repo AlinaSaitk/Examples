61b5fe28f685081ef834a85848fce1b59b0db0f9
Alina <aax_74@mail.ru>
2022-10-17 19:50:22 +0500

    add c#_8_62_dz еще не решена

--- c#_8_62_dz/Program.cs
+++ c#_8_62_dz/Program.cs
@@ -5,3 +5,23 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int[,] SpiralArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+
+int[,] spiral = SpiralArray(3, 4);
+Print2DArray(spiral);
